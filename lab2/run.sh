e3443347032d092727d795f4d593ffb0beb31d80
Moritz Bachmann <moritz.bachmann@student.kit.edu>
2023-12-01 10:38:17 +0100

    Testruns and result uB2

---
+++ lab2/run.sh
@@ -0,0 +1,75 @@
+echo "1024 2thread"
+./gameoflife 1024 1024 500 2 1
+./gameoflife 1024 1024 500 2 1
+./gameoflife 1024 1024 500 2 1
+./gameoflife 1024 1024 500 2 1
+./gameoflife 1024 1024 500 2 1
+echo "echo 2048 2thread"
+./gameoflife 2048 2048 500 2 1
+./gameoflife 2048 2048 500 2 1
+./gameoflife 2048 2048 500 2 1
+./gameoflife 2048 2048 500 2 1
+./gameoflife 2048 2048 500 2 1
+echo "echo 4096 2thread"
+./gameoflife 4096 4096 500 2 1
+./gameoflife 4096 4096 500 2 1
+./gameoflife 4096 4096 500 2 1
+./gameoflife 4096 4096 500 2 1
+./gameoflife 4096 4096 500 2 1
+echo "1024 4thread"
+./gameoflife 1024 1024 500 2 2
+./gameoflife 1024 1024 500 2 2
+./gameoflife 1024 1024 500 2 2
+./gameoflife 1024 1024 500 2 2
+./gameoflife 1024 1024 500 2 2
+echo "echo 2048 4thread"
+./gameoflife 2048 2048 500 2 2
+./gameoflife 2048 2048 500 2 2
+./gameoflife 2048 2048 500 2 2
+./gameoflife 2048 2048 500 2 2
+./gameoflife 2048 2048 500 2 2
+echo "echo 4096 4thread"
+./gameoflife 4096 4096 500 2 2
+./gameoflife 4096 4096 500 2 2
+./gameoflife 4096 4096 500 2 2
+./gameoflife 4096 4096 500 2 2
+./gameoflife 4096 4096 500 2 2
+
+echo "1024 8thread"
+./gameoflife 1024 1024 500 4 2
+./gameoflife 1024 1024 500 4 2
+./gameoflife 1024 1024 500 4 2
+./gameoflife 1024 1024 500 4 2
+./gameoflife 1024 1024 500 4 2
+echo "echo 2048 8thread"
+./gameoflife 2048 2048 500 4 2
+./gameoflife 2048 2048 500 4 2
+./gameoflife 2048 2048 500 4 2
+./gameoflife 2048 2048 500 4 2
+./gameoflife 2048 2048 500 4 2
+echo "echo 4096 8thread"
+./gameoflife 4096 4096 500 4 2
+./gameoflife 4096 4096 500 4 2
+./gameoflife 4096 4096 500 4 2
+./gameoflife 4096 4096 500 4 2
+./gameoflife 4096 4096 500 4 2
+
+
+echo "1024 16thread"
+./gameoflife 1024 1024 500 4 4
+./gameoflife 1024 1024 500 4 4
+./gameoflife 1024 1024 500 4 4
+./gameoflife 1024 1024 500 4 4
+./gameoflife 1024 1024 500 4 4
+echo "echo 2048 16thread"
+./gameoflife 2048 2048 500 4 4
+./gameoflife 2048 2048 500 4 4
+./gameoflife 2048 2048 500 4 4
+./gameoflife 2048 2048 500 4 4
+./gameoflife 2048 2048 500 4 4
+echo "echo 4096 16thread"
+./gameoflife 4096 4096 500 4 4
+./gameoflife 4096 4096 500 4 4
+./gameoflife 4096 4096 500 4 4
+./gameoflife 4096 4096 500 4 4
+./gameoflife 4096 4096 500 4 4
